857657e6cb82a4f6fdc19de16e8b8d2774e4e344
Will <forbes8963@gmail.com>
2020-07-21 08:16:26 -0400

    Add styled components

--- src/MovieDetail.js
+++ src/MovieDetail.js
@@ -1,6 +1,9 @@
 import  React, { Component } from 'react';
+import styled from 'styled-components';
 
 import './App.css';
+
+import { Poster } from './movie';
 
 class MovieDetail extends Component {
 
@@ -20,17 +23,45 @@ class MovieDetail extends Component {
 		}
 	}
 	render() {
-		const { backdrop_path, title, release_date, overview } = this.state.movie
-		const POSTER_PATH = 'http://image.tmdb.org/t/p/w1280';
+		const { backdrop_path, poster_path, title, release_date, overview } = this.state.movie
+
+		const backdrop = `http://image.tmdb.org/t/p/w1280${backdrop_path}`;
+		const poster = `http://image.tmdb.org/t/p/w154${poster_path}`;
+
 		return (
-      <div>
-				<img src={`${POSTER_PATH}${backdrop_path}`} alt={title}/>
-        <h1>{title}</h1>
-				<h3>{release_date}</h3>
-				<p>{overview}</p>
-      </div>
+      <MovieWrapper backdrop={backdrop}>
+				<MovieInfo>
+					<Poster src={poster} alt={title}/>
+					<div>
+						<h1>{title}</h1>
+						<h3>{release_date}</h3>
+						<p>{overview}</p>
+					</div>
+				</MovieInfo>
+      </MovieWrapper>
 		);
 	}
 }
 
 export default MovieDetail; 
+
+const MovieWrapper = styled.div`
+	position: relative;
+	padding-top: 50vh;
+	background: url(${props => props.backdrop}) no-repeat;
+	background-size: cover;
+`;
+
+const MovieInfo = styled.div`
+	background: white;
+	text-align: left;
+	padding: 2rem 10%;
+	display: flex;
+	> div {
+		margin-left: 20px;
+	}
+	img {
+		position: relative;
+		top: -5rem;
+	}
+`;
